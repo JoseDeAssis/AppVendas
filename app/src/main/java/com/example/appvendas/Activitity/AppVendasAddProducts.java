2a343d9b1c6c62b74cb34d64e67ea83b8d539c97
JoseDeAssis <jose.assis@aluno.uece.br>
2019-10-21 12:34:51 -0300

    Criação de um Dialog para a tela de addProduto

--- app/src/main/java/com/example/appvendas/Activitity/AppVendasAddProducts.java
+++ app/src/main/java/com/example/appvendas/Activitity/AppVendasAddProducts.java
@@ -6,6 +6,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.cardview.widget.CardView;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.os.Bundle;
@@ -15,9 +16,11 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.example.appvendas.R;
 import com.google.android.material.card.MaterialCardView;
+import com.google.android.material.dialog.MaterialAlertDialogBuilder;
 import com.google.android.material.textfield.TextInputEditText;
 
 public class AppVendasAddProducts extends AppCompatActivity {
@@ -76,7 +79,22 @@ public class AppVendasAddProducts extends AppCompatActivity {
 
         switch (item.getItemId()) {
             case R.id.confirmIcon:
-                finish();
+                new MaterialAlertDialogBuilder(this, R.style.Theme_MaterialComponents_Light_Dialog)
+                        .setTitle("Salvar produto?")
+                        .setMessage("Ao salvar o produto ele aparecerá em alguma das tabs da tela principal")
+                        .setPositiveButton("Accept", /* listener = */ new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                Toast.makeText(AppVendasAddProducts.this, "Show de bola", Toast.LENGTH_SHORT).show();
+                            }
+                        })
+                        .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                Toast.makeText(AppVendasAddProducts.this, "Mó paia man", Toast.LENGTH_SHORT).show();
+                            }
+                        }).show();
+//                finish();
                 break;
         }
 
